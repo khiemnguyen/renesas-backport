11c34fa27ee1c972019671927b96f122f63da184
Grant Likely <grant.likely@secretlab.ca>
2012-05-17 13:54:40 -0600

    gpio: Adjust of_xlate API to support multiple GPIO chips
    This patch changes the of_xlate API to make it possible for multiple
    gpio_chips to refer to the same device tree node.  This is useful for
    banked GPIO controllers that use multiple gpio_chips for a single
    device.  With this change the core code will try calling of_xlate on
    each gpio_chip that references the device_node and will return the
    gpio number for the first one to return 'true'.
    
    Tested-by: Roland Stigge <stigge@antcom.de>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>
    (cherry picked from commit 3d0f7cf0f3633f92ddeb767eb59cab73963d4dee)
    
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -1158,7 +1158,7 @@ EXPORT_SYMBOL_GPL(gpiochip_remove);
  */
 struct gpio_chip *gpiochip_find(const void *data,
 				int (*match)(struct gpio_chip *chip,
-					     const void *data))
+					     void *data))
 {
 	struct gpio_chip *chip = NULL;
 	unsigned long flags;
