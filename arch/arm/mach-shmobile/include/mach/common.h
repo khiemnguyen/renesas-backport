3d24235d82e4daa74815dad5311b8f6c65a8737f
Rafael J. Wysocki <rjw@sisk.pl>
2012-08-15 20:57:27 +0200

    ARM: shmobile: Move definition of shmobile_init_late() to header
    The role of the only function in the common.c file in
    arch/arm/mach-shmobile, shmobile_init_late(), is to call two
    initializers whose definitions depend on kernel configuration
    options.  Those initializers may very well be called from a static
    inline function in arm/mach-shmobile/include/mach/common.h,
    though, in which makes the code a bit easier to read.  Moreover,
    the common.c may be dropped entirely then.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Acked-by: Magnus Damm <damm@opensource.se>
    (cherry picked from commit 35f2b0bd5911dc0eef3f5352b6acb79c69420111)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    
    Conflicts:
    	arch/arm/mach-shmobile/include/mach/common.h

--- arch/arm/mach-shmobile/include/mach/common.h
+++ arch/arm/mach-shmobile/include/mach/common.h
@@ -83,8 +83,6 @@ extern void r8a7740_meram_workaround(void);
 
 extern void r8a7779_register_twd(void);
 
-extern void shmobile_init_late(void);
-
 #ifdef CONFIG_SUSPEND
 int shmobile_suspend_init(void);
 #else
@@ -108,5 +106,11 @@ static inline int shmobile_cpu_is_dead(unsigned int cpu) { return 1; }
 
 extern void __iomem *shmobile_scu_base;
 extern void shmobile_smp_init_cpus(unsigned int ncores);
+
+static inline void shmobile_init_late(void)
+{
+	shmobile_suspend_init();
+	shmobile_cpuidle_init();
+}
 
 #endif /* __ARCH_MACH_COMMON_H */
