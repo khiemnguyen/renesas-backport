01c3895703d18808c91c5875e53a7a160b68618b
Linus Walleij <linus.walleij@linaro.org>
2012-11-06 14:49:39 +0100

    gpiolib: fix up function prototypes etc
    Commit 69e1601bca88809dc118abd1becb02c15a02ec71
    "gpiolib: provide provision to register pin ranges"
    
    Got most of it's function prototypes wrong, so fix this up by:
    
    - Moving the void declarations into static inlines in
      <linux/gpio.h> (previously the actual prototypes were declared
      here...)
    
    - Declare the gpiochip_add_pin_range() and
      gpiochip_remove_pin_ranges() functions in <asm-generic/gpio.h>
      together with the pin range struct declaration itself.
    
    - Actually only implement these very functions in gpiolib.c
      if CONFIG_PINCTRL is set.
    
    - Additionally export the symbols since modules will need to
      be able to do this.
    
    Reviewed-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    (cherry picked from commit 165adc9c1734a3f3bdbc6dc7c7a29bbefb424006)
    
    Conflicts:
    	include/linux/gpio.h
    
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- include/linux/gpio.h
+++ include/linux/gpio.h
@@ -40,7 +40,7 @@ struct gpio {
 #ifdef CONFIG_GENERIC_GPIO
 #include <asm/gpio.h>
 
-#else
+#else /* ! CONFIG_GENERIC_GPIO */
 
 #include <linux/kernel.h>
 #include <linux/types.h>
@@ -183,9 +183,21 @@ static inline int irq_to_gpio(unsigned irq)
 	return -EINVAL;
 }
 
-void gpiochip_add_pin_range(struct gpio_chip *chip, const char *pinctl_name,
-		unsigned int pin_base, unsigned int npins);
-void gpiochip_remove_pin_ranges(struct gpio_chip *chip);
-#endif
+#ifdef CONFIG_PINCTRL
+
+static inline void
+gpiochip_add_pin_range(struct gpio_chip *chip, const char *pinctl_name,
+		       unsigned int pin_base, unsigned int npins)
+{
+}
+
+static inline void
+gpiochip_remove_pin_ranges(struct gpio_chip *chip)
+{
+}
+
+#endif /* CONFIG_PINCTRL */
+
+#endif /* ! CONFIG_GENERIC_GPIO */
 
 #endif /* __LINUX_GPIO_H */
