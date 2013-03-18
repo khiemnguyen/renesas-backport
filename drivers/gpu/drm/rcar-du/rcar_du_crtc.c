a810a2b1c120b829465c36da247050b3e8e99232
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2013-03-18 10:30:17 +0100

    drm/rcar-du: Centralize DU device core resource management
    Create reference-counted get/put handlers for the DU device. The get()
    operation enables the device clock and initializes core registers.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    (cherry picked from commit 1b3c347473a4195770414af3e13bbff24e336bd8)
    
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- drivers/gpu/drm/rcar-du/rcar_du_crtc.c
+++ drivers/gpu/drm/rcar-du/rcar_du_crtc.c
@@ -232,14 +232,7 @@ static void rcar_du_crtc_start(struct rcar_du_crtc *rcrtc)
 	if (WARN_ON(rcrtc->plane->format == NULL))
 		return;
 
-	/* Enable clocks before accessing the hardware. */
-	clk_prepare_enable(rcdu->clock);
-
-	/* Enable extended features */
-	rcar_du_write(rcdu, DEFR, DEFR_CODE | DEFR_DEFE);
-	rcar_du_write(rcdu, DEFR2, DEFR2_CODE | DEFR2_DEFE2G);
-	rcar_du_write(rcdu, DEFR3, DEFR3_CODE | DEFR3_DEFE3);
-	rcar_du_write(rcdu, DEFR4, DEFR4_CODE);
+	rcar_du_get(rcdu);
 
 	/* Set display off and background to black */
 	rcar_du_crtc_write(rcrtc, DOOR, DOOR_RGB(0, 0, 0));
@@ -297,7 +290,7 @@ static void rcar_du_crtc_stop(struct rcar_du_crtc *rcrtc)
 
 	rcar_du_start_stop(rcdu, false);
 
-	clk_disable_unprepare(rcdu->clock);
+	rcar_du_put(rcdu);
 
 	rcrtc->started = false;
 }
