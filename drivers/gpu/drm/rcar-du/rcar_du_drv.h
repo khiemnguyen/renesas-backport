fa548c9cbc1f2296787abea34baf57eaf0b0937d
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2013-06-14 14:15:01 +0200

    drm/rcar-du: Support per-CRTC clock and IRQ
    Some of the DU revisions use one clock and IRQ per CRTC instead of one
    clock and IRQ per device. Retrieve the correct clock and register the
    correct IRQ for each CRTC.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    (cherry picked from commit 3d5d34c423c3c33d6e2fea16e4c37fc695a7af8b)
    
    Conflicts:
    
    	drivers/gpu/drm/rcar-du/rcar_du_drv.c
    
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- drivers/gpu/drm/rcar-du/rcar_du_drv.h
+++ drivers/gpu/drm/rcar-du/rcar_du_drv.h
@@ -26,6 +26,8 @@ struct device;
 struct drm_device;
 struct drm_fbdev_cma;
 
+#define RCAR_DU_FEATURE_CRTC_IRQ_CLOCK	(1 << 0)	/* Per-CRTC IRQ and clock */
+
 /*
  * struct rcar_du_device_info - DU model-specific information
  * @features: device features (RCAR_DU_FEATURE_*)
@@ -40,7 +42,6 @@ struct rcar_du_device {
 	const struct rcar_du_device_info *info;
 
 	void __iomem *mmio;
-	struct clk *clock;
 	unsigned int use_count;
 
 	struct drm_device *ddev;
