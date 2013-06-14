82a97038348cfa11330d38a361ba0ce0930b98be
Shinobu Uehara <shinobu.uehara.xc@renesas.com>
2013-06-14 15:02:10 +0900

    mmc: tmio: Add busy check bit switch option
    Signed-off-by: Shinobu Uehara <shinobu.uehara.xc@renesas.com>
    (cherry picked from commit bd4bbe0a6bf03f7127b60ea01cf1bd6c7b64e44d)
    
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>

--- drivers/mmc/host/tmio_mmc_pio.c
+++ drivers/mmc/host/tmio_mmc_pio.c
@@ -470,6 +470,7 @@ void tmio_mmc_do_data_irq(struct tmio_mmc_host *host)
 static void tmio_mmc_data_irq(struct tmio_mmc_host *host)
 {
 	struct mmc_data *data;
+	struct tmio_mmc_data *pdata = host->pdata;
 	spin_lock(&host->lock);
 	data = host->data;
 
@@ -485,9 +486,20 @@ static void tmio_mmc_data_irq(struct tmio_mmc_host *host)
 		 * DATAEND interrupt with the BUSY bit set, in this cases
 		 * waiting for one more interrupt fixes the problem.
 		 */
-		if (!(sd_ctrl_read32(host, CTL_STATUS) & TMIO_STAT_CMD_BUSY)) {
-			tmio_mmc_disable_mmc_irqs(host, TMIO_STAT_DATAEND);
-			tasklet_schedule(&host->dma_complete);
+		if (pdata->flags & TMIO_MMC_CHECK_ILL_FUNC) {
+			if ((sd_ctrl_read32(host, CTL_STATUS)
+			& TMIO_STAT_ILL_FUNC)) {
+				tmio_mmc_disable_mmc_irqs(host,
+					TMIO_STAT_DATAEND);
+				tasklet_schedule(&host->dma_complete);
+			}
+		} else {
+			if (!(sd_ctrl_read32(host, CTL_STATUS)
+			& TMIO_STAT_CMD_BUSY)) {
+				tmio_mmc_disable_mmc_irqs(host,
+					TMIO_STAT_DATAEND);
+				tasklet_schedule(&host->dma_complete);
+			}
 		}
 	} else if (host->chan_rx && (data->flags & MMC_DATA_READ) && !host->force_pio) {
 		tmio_mmc_disable_mmc_irqs(host, TMIO_STAT_DATAEND);
