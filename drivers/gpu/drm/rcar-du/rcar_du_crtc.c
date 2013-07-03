f0379789d2cc05d22be4b0039c54a912b4a13c67
Simon Horman <horms+renesas@verge.net.au>
2013-07-03 13:38:27 +0900

    Revert "drm/rcar-du: Update plane format after releasing hardware planes"
    This reverts commit 4e6f7c21f4b847d6a1bb1c0f5afa5204bb565e9c.

--- drivers/gpu/drm/rcar-du/rcar_du_crtc.c
+++ drivers/gpu/drm/rcar-du/rcar_du_crtc.c
@@ -293,7 +293,6 @@ static int rcar_du_crtc_mode_set(struct drm_crtc *crtc,
 	struct rcar_du_device *rcdu = crtc->dev->dev_private;
 	struct rcar_du_crtc *rcrtc = to_rcar_crtc(crtc);
 	const struct rcar_du_format_info *format;
-	int ret;
 
 	format = rcar_du_format_info(crtc->fb->pixel_format);
 	if (format == NULL) {
@@ -301,10 +300,6 @@ static int rcar_du_crtc_mode_set(struct drm_crtc *crtc,
 			crtc->fb->pixel_format);
 		return -EINVAL;
 	}
-
-	ret = rcar_du_plane_reserve(rcrtc->plane, format);
-	if (ret < 0)
-		return ret;
 
 	rcrtc->plane->format = format;
 	rcrtc->plane->pitch = crtc->fb->pitches[0];
@@ -316,7 +311,7 @@ static int rcar_du_crtc_mode_set(struct drm_crtc *crtc,
 
 	rcar_du_plane_compute_base(rcrtc->plane, crtc->fb);
 
-	return 0;
+	return rcar_du_plane_reserve(rcrtc->plane);
 }
 
 static void rcar_du_crtc_mode_commit(struct drm_crtc *crtc)
