23d951cf8fcc7147a6ba25bd9ce6db1dc0810e8b
Magnus Damm <damm@opensource.se>
2013-10-01 17:13:16 +0900

    ARM: shmobile: r8a7791 SMP support
    Tie in the APMU SMP code on r8a7791. When used together
    with the secondary CPU device node and smp_ops in the
    board specific code then this will allow use of the
    two Cortex-A15 cores in the r8a7791 SoC.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    (cherry picked from commit 2349199db55bfb99e2ebdce285a7e297ba63a7ef)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    
    Conflicts:
    	arch/arm/mach-shmobile/Makefile

--- arch/arm/mach-shmobile/setup-r8a7791.c
+++ arch/arm/mach-shmobile/setup-r8a7791.c
@@ -176,6 +176,7 @@ static const char *r8a7791_boards_compat_dt[] __initdata = {
 };
 
 DT_MACHINE_START(R8A7791_DT, "Generic R8A7791 (Flattened Device Tree)")
+	.smp		= smp_ops(r8a7791_smp_ops),
 	.init_early	= r8a7791_init_early,
 	.timer		= &rcar_gen2_timer,
 	.dt_compat	= r8a7791_boards_compat_dt,
