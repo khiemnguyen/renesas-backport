bb14b3e6ed08dc2c43de4d1cf8b8dd1e63223758
Ryo Kataoka <ryo.kataoka.wt@renesas.com>
2013-11-12 18:27:48 +0900

    Revert "drm/rcar-du: Restart the DU group when a plane source changes"
    This reverts commit 323cf8559256268584809544f16fc07967368e1d.

--- drivers/gpu/drm/rcar-du/rcar_du_plane.h
+++ drivers/gpu/drm/rcar-du/rcar_du_plane.h
@@ -76,7 +76,6 @@ struct rcar_du_plane {
 struct rcar_du_planes {
 	struct rcar_du_plane planes[RCAR_DU_NUM_SW_PLANES];
 	unsigned int free;
-	bool need_restart;
 	struct mutex lock;
 
 	struct drm_property *alpha;
