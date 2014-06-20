0baee21b5149bff1f5789f6e8603cf26800ac67e
Geert Uytterhoeven <geert+renesas@glider.be>
2014-06-20 18:53:06 +0200

    ARM: shmobile: Move r8a7790.h
    Change location of r8a7790.h so it can be included as "r8a7790.h"
    instead of the old style <mach/r8a7790.h>
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    (cherry picked from commit fccae89318fda71012f96042dd971a6f9bf0904e)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    
    Conflicts:
    	arch/arm/mach-shmobile/board-lager-reference.c

--- arch/arm/mach-shmobile/board-lager-reference.c
+++ arch/arm/mach-shmobile/board-lager-reference.c
@@ -28,13 +28,14 @@
 #include <linux/usb/phy.h>
 #include <linux/usb/renesas_usbhs.h>
 #include <linux/platform_data/vsp1.h>
-#include <mach/r8a7790.h>
 #include <asm/mach/arch.h>
 #include <sound/rcar_snd.h>
 #include <sound/simple_card.h>
+
 #include "clock.h"
 #include "common.h"
 #include "irqs.h"
+#include "r8a7790.h"
 #include "rcar-gen2.h"
 
 /* DU */
